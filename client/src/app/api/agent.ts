8dd6a6b5f98e73847ff694a2b7e75d0570925dda
link05 <dahalamit4@gmail.com>
2023-03-25 14:13:06 -0700

    done w/ 52

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,4 +1,7 @@
+import { BreakfastDiningOutlined } from "@mui/icons-material";
+import { breadcrumbsClasses } from "@mui/material";
 import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
 
 axios.defaults.baseURL = 'http://localhost:5000/api';
 
@@ -11,8 +14,22 @@ const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.response.use(response => {
     return response;
-}, (error:AxiosError) => {
-    console.log('caught by interceptor');
+}, (error:AxiosError) =>
+{
+    const {data,status} = error.response as AxiosResponse;
+    switch(status){
+        case 400:
+            toast.error(data.title);
+            break;
+        case 401:
+            toast.error(data.title);
+            break;
+        case 500:
+            toast.error(data.title);
+            break;
+        default:
+            break;
+    }
     return Promise.reject(error.response);
 })
 
