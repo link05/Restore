5e589c706148ff853a5ae08063db89d0ba9d4e7f
link05 <dahalamit4@gmail.com>
2023-04-01 12:30:36 -0700

    done with 88

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -1,7 +1,19 @@
-import { createStore } from "redux";
-import CounterReducer from "../../features/contact/counterReducer";
+import { useSelect } from "@mui/base";
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { counterSlice } from "../../features/contact/counterSlice";
 
 
-export function configureStore() {
-    return createStore(CounterReducer)
-}
+
+export const store = configureStore({
+    reducer: {
+        counter: counterSlice.reducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
