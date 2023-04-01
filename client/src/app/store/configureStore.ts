a6d0d69496123c1a10f24a09a5acb7155fc694f9
link05 <dahalamit4@gmail.com>
2023-04-01 14:46:41 -0700

    done with 91

--- client/src/app/store/configureStore.ts
+++ client/src/app/store/configureStore.ts
@@ -5,17 +5,18 @@ import { basketSlice } from "../../features/basket/basketSlice";
 import { counterSlice } from "../../features/contact/counterSlice";
 
 
-
+//list all slices here
 export const store = configureStore({
+    //reducers contain state and methods that update the states
     reducer: {
         counter: counterSlice.reducer,
         basket: basketSlice.reducer
     }
 })
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>; //state
+export type AppDispatch = typeof store.dispatch; //dispatch
 
-
+//methods used by components to get/modify states
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
