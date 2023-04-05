9520408d25c9e16ec566edcca534b4aeb9802d91
link05 <dahalamit4@gmail.com>
2023-04-05 01:11:51 -0700

    done with 109

--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -3,18 +3,23 @@ import agent from "../../app/api/agent";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { Product } from "../../app/models/product";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { fetchProductsAsync, productSelectors } from "./catalogSlice";
+import { fetchFilters, fetchProductsAsync, productSelectors } from "./catalogSlice";
 import ProductList from "./ProductList";
 
 export default function Catalog() {
     //onst [products, setProducts] = useState<Product[]>([]);
     const products = useAppSelector(productSelectors.selectAll);
     const dispatch = useAppDispatch();
-    const {productsLoaded, status} = useAppSelector(state => state.catalog);
+    const {productsLoaded, status, filtersLoaded} = useAppSelector(state => state.catalog);
 
     useEffect(() => {
         if(!productsLoaded) dispatch(fetchProductsAsync());
-    }, [productsLoaded])
+    }, [productsLoaded, dispatch])
+
+
+    useEffect(() => {
+        if(!filtersLoaded) dispatch(fetchFilters());
+    },[dispatch, filtersLoaded])
 
     if(status.includes('pending')) return <LoadingComponent message="Loading components..."/>
 
